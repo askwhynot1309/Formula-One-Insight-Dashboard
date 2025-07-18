443522714e590d6259039380ad7b1e03d00ba390
AskWhyNot <109494300+askwhynot1309@users.noreply.github.com>
2025-07-18 18:45:06 +0700

    Update Program.cs

--- F1API/Program.cs
+++ F1API/Program.cs
@@ -101,11 +101,12 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+    
+// }
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
